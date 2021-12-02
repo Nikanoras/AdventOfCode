6a73c76272a589274542f689e491fb213eb86cad
Nikanoras Zavadskij <z.nikanoras@gmail.com>
2021-12-02 18:40:26 +0200

    Day1: Create single depth increase calculator

--- 2021/Day1/Day1.App/Program.cs
+++ 2021/Day1/Day1.App/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Day1.App;
+
+var depthMeasurements = File.ReadAllLines("./input.txt").Select(d => int.Parse(d)).ToArray();
+
+var depthIncreaseCalculator = new DepthIncreaseCalculator();
+
+int timesDepthIncreased = depthIncreaseCalculator.CalculateTimesDepthIncreased(depthMeasurements);
+
+Console.WriteLine($"How many measurements are larger than the previous measurement?");
+Console.WriteLine($"{timesDepthIncreased} times");
