405428c3ef8c6135a36f2b67fd09613ab64d0bd3
Nikanoras Zavadskij <37303011+Nikanoras@users.noreply.github.com>
2022-12-01 18:58:04 +0200

    Add 2022 Day 1 full solution

---
+++ 2022/Day1/Day1.App/Program.cs
@@ -0,0 +1,18 @@
+﻿var calories = File.ReadAllLines("./input.txt");
+var eachElfSummedCalories = new SortedSet<int>();
+var currentElfCalorieSum = 0;
+foreach (var calory in calories)
+{
+    if (int.TryParse(calory, out int parsedCalory))
+    {
+        currentElfCalorieSum += parsedCalory;
+    }
+    else
+    {
+        eachElfSummedCalories.Add(currentElfCalorieSum);
+        currentElfCalorieSum = 0;
+    }
+}
+
+Console.WriteLine($"Top 1 sum of calories carried by elf: {eachElfSummedCalories.Last()}");
+Console.WriteLine($"Top 3 sum of calories carried by elfs summed: {eachElfSummedCalories.TakeLast(3).Sum()}");
