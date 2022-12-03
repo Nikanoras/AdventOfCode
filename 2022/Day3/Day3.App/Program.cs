66b8c2a8d7b7b264a9945eb8cb77fc4896e1df0a
Nikanoras Zavadskij <37303011+Nikanoras@users.noreply.github.com>
2022-12-03 19:46:20 +0200

    Add solution for day 3

---
+++ 2022/Day3/Day3.App/Program.cs
@@ -0,0 +1,49 @@
+﻿string[] ruckSacks = File.ReadAllLines("input.txt");
+
+var firstResult = 0;
+foreach (var ruckSackItems in ruckSacks)
+{
+    var halfLength = ruckSackItems.Length / 2;
+    var firstCompartment = ruckSackItems[0..halfLength].ToCharArray();
+    var secondCompartment = ruckSackItems[halfLength..ruckSackItems.Length].ToCharArray();
+
+    var sameChars = GetDistinctCharsBetweenArrays(firstCompartment, secondCompartment);
+    firstResult += ConvertCharsToPriorityPoints(sameChars);
+}
+Console.WriteLine(firstResult);
+
+var secondResult = 0;
+for (int i = 2; i < ruckSacks.Length; i += 3)
+{
+    var firstElfRucksack = ruckSacks[i - 2].ToCharArray();
+    var secondElfRucksack = ruckSacks[i - 1].ToCharArray();
+    var thirdElfRucksack = ruckSacks[i].ToCharArray();
+
+    var sameChars = GetDistinctCharsBetweenArrays(firstElfRucksack, secondElfRucksack, thirdElfRucksack);
+    secondResult += ConvertCharsToPriorityPoints(sameChars);
+}
+Console.WriteLine(secondResult);
+
+static int ConvertCharsToPriorityPoints(List<char> sameChars)
+{
+    return sameChars.Select(c => char.IsUpper(c) ? c - 38 : c - 96).Sum();
+}
+
+static List<char> GetDistinctCharsBetweenArrays(params char[][] charArrays)
+{
+    if (!charArrays.Any())
+    {
+        throw new ArgumentException("No chars passed");
+    }
+
+    var compareTo = charArrays[0].AsEnumerable();
+
+    foreach (var item in charArrays.Skip(1))
+    {
+        compareTo = compareTo.Where(c => item.Contains(c));
+    }
+
+    var result = compareTo.Distinct().ToList();
+    return result;
+}
+
