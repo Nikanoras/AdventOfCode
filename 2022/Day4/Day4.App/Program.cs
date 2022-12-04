76f453de4d5f179ae5f2161621e523f0c2c21c07
Nikanoras Zavadskij <37303011+Nikanoras@users.noreply.github.com>
2022-12-04 11:10:26 +0200

    Add solution for day 4 all parts

---
+++ 2022/Day4/Day4.App/Program.cs
@@ -0,0 +1,44 @@
+﻿string[] elfPairs = File.ReadAllLines("input.txt");
+var firstCount = 0;
+var secondCount = 0;
+foreach (var elfPair in elfPairs)
+{
+    string[] assignments = elfPair.Split(",");
+    var firstElfAssignment = GetAssignmentStartAndEndSectionIds(assignments[0]);
+    var secondElfAssignment = GetAssignmentStartAndEndSectionIds(assignments[1]);
+
+    var smallestSectionId = Math.Min(firstElfAssignment.startSectionId, secondElfAssignment.startSectionId);
+    var largestSectionId = Math.Max(firstElfAssignment.endSectionId, secondElfAssignment.endSectionId);
+
+    var hasFirstLowestSectionId = smallestSectionId == firstElfAssignment.startSectionId;
+    var hasSecondLowestSectionId = smallestSectionId == secondElfAssignment.startSectionId;
+
+    if (hasFirstLowestSectionId && largestSectionId == firstElfAssignment.endSectionId)
+    {
+        firstCount++;
+    }
+    else if (hasSecondLowestSectionId && largestSectionId == secondElfAssignment.endSectionId)
+    {
+        firstCount++;
+    }
+
+    if (hasFirstLowestSectionId && secondElfAssignment.startSectionId <= firstElfAssignment.endSectionId)
+    {
+        secondCount++;
+    }
+    else if (hasSecondLowestSectionId && firstElfAssignment.startSectionId <= secondElfAssignment.endSectionId)
+    {
+        secondCount++;
+    }
+}
+
+Console.WriteLine($"In how many assignment pairs does one range fully contain the other?: {firstCount}");
+Console.WriteLine($"In how many assignment pairs do the ranges overlap?: {secondCount}");
+
+
+static (int startSectionId, int endSectionId) GetAssignmentStartAndEndSectionIds(string assignment)
+{
+    var splitRange = assignment.Split("-");
+
+    return (int.Parse(splitRange[0]), int.Parse(splitRange[1]));
+}
